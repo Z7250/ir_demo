35be4ece23aa5da2dec3f91f12ce2f5f2bd96793
ZhangGY <zhanggy725@outlook.com>
2026-05-11 11:39:29 +0800

    1.补充了文章标题、作者、发表时间、博文内容、标签、分类、文章URL地址索引字段 2.删除不需要的类

--- src/main/java/cn/edu/bistu/cs/ir/index/LucenePipeline.java
+++ src/main/java/cn/edu/bistu/cs/ir/index/LucenePipeline.java
@@ -3,6 +3,7 @@ package cn.edu.bistu.cs.ir.index;
 import cn.edu.bistu.cs.ir.crawler.CnBlogsCrawler;
 //import cn.edu.bistu.cs.ir.crawler.SinaBlogCrawler;
 import cn.edu.bistu.cs.ir.model.Blog;
+import cn.edu.bistu.cs.ir.model.BlogStats;
 import org.apache.lucene.document.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,7 +49,33 @@ public class LucenePipeline implements Pipeline {
         document.add(new TextField("TITLE", blog.getTitle(), Field.Store.YES));
         //页面内容全文
         document.add(new TextField("CONTENT", blog.getContent(), Field.Store.YES));
-        //TODO 下面请同学们补充其他的待检索字段，如发布时间、标签、作者等，并思考应该选择什么字段类型
+        //作者
+        document.add(new StringField("AUTHOR", blog.getAuthor(), Field.Store.YES));
+        //发表时间
+        document.add(new LongPoint("DATE", blog.getDate()));
+        document.add(new StoredField("DATE", blog.getDate()));
+        //标签
+        if(blog.getTags() != null){
+            for(String tag : blog.getTags()){
+                document.add(new StringField("TAG", tag.trim(), Field.Store.YES));
+            }
+        }
+        //文章URL地址
+        String url = String.format("https://www.cnblogs.com/%s/p/%s.html",
+                blog.getAuthor() != null ? blog.getAuthor() : "", blog.getId());
+        document.add(new StringField("URL", url, Field.Store.YES));
+        //博文动态信息(BlogStats)
+        BlogStats stats = blog.getBlogStats();
+        if(stats != null){
+            document.add(new IntPoint("VIEW_COUNT", stats.getViewCount()));
+            document.add(new StoredField("VIEW_COUNT", stats.getViewCount()));
+            document.add(new IntPoint("FEEDBACK_COUNT", stats.getFeedbackCount()));
+            document.add(new StoredField("FEEDBACK_COUNT", stats.getFeedbackCount()));
+            document.add(new IntPoint("DIGG_COUNT", stats.getDiggCount()));
+            document.add(new StoredField("DIGG_COUNT", stats.getDiggCount()));
+            document.add(new IntPoint("BURY_COUNT", stats.getBuryCount()));
+            document.add(new StoredField("BURY_COUNT", stats.getBuryCount()));
+        }
         return document;
     }
 }
